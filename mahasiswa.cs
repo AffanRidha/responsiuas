a31d1920f592f7955908b7ffcf22a8cbaf448f30
AffanRidha <affanridha@students.amikom.ac.id>
2021-07-13 15:46:33 +0700

    origin

---
+++ mahasiswa.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectMahasiswa
+{
+    public class Mahasiswa
+    {
+        public string nim { get; set; }
+        public string nama { get; set; }
+        public string Kelamin { get; set; }
+        public string total { get; set; }
+
+    }
+}
